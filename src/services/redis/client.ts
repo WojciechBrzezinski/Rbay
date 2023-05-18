600eb463221d85d95cb73922d3c63b801e1e13e1
Wojtek <w.brzezinski@ucodepolska.pl>
2023-05-18 15:14:30 +0200

    Aktualizacja ilości wyświetleń skryptem LUA

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,12 +1,56 @@
-import { createClient } from 'redis';
+import { createClient, defineScript } from 'redis';
+import { itemsByViewsKey, itemsKey, itemsViewsKey } from "$services/keys";
 
 const client = createClient({
-	socket: {
-		host: process.env.REDIS_HOST,
-		port: parseInt(process.env.REDIS_PORT)
-	},
-	password: process.env.REDIS_PW
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT)
+    },
+    password: process.env.REDIS_PW,
+    scripts: {
+        addOneAndStore: defineScript({
+            NUMBER_OF_KEYS: 1,
+            SCRIPT: `
+				return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+			`,
+            transformArguments(key: string, value: number) {
+                return [key, value.toString()]
+            },
+            transformReply(reply: any) {
+                return reply
+            }
+        }),
+        incrementView: defineScript({
+            NUMBER_OF_KEYS: 3,
+            SCRIPT: `
+                local itemsViewsKey = KEYS[1]
+                local itemsKey = KEYS[2]
+                local itemsByViewsKey = KEYS[3]
+
+                local itemId = ARGV[1]
+                local userId = ARGV[2]
+
+                local inserted = redis.call('PFADD', itemsViewsKey, userId)
+
+                if inserted == 1 then
+                    redis.call('HINCRBY', itemsKey, 'views', 1)
+                    redis.call('ZINCRBY', itemsByViewsKey, 1, itemId)
+                end
+            `,
+            transformArguments(itemId: string, userId: string) {
+                return [itemsViewsKey(itemId), itemsKey(itemId), itemsByViewsKey(), itemId, userId]
+            },
+            transformReply() {
+            }
+        })
+    }
 });
+
+// client.on('connect', async () => {
+//     await client.addOneAndStore('books:count', 10)
+//     const result = await client.get('books:count')
+//     console.log(result)
+// })
 
 client.on('error', (err) => console.error(err));
 client.connect();
